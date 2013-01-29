f9d0f3447e1c43cbd11c5acaa399923b9d758818
Leandro Souza <leandro.asouza@spread.com.br>
2013-01-29 17:15:40 -0200

    Capítulo 9 - Threads

--- src/br/com/cert9/Testthread.java
+++ src/br/com/cert9/Testthread.java
@@ -10,13 +10,13 @@ package br.com.cert9;
  * 2. Executável: já foi chamado o start(). Portanto o sheduler pode levá-lo ao estado ativo a qualquer momento
  * 3. Ativo (Executando): estado do thread em execução. Àquele que foi chamado pelo sheduler e estiver executando. 
  * 
- * Meios de "tentar" (nï¿½o hï¿½ garantias) de retirar um Thread de execuï¿½ï¿½o:
- * yield() -> solicita que o sheduler retire o thread em execuï¿½ï¿½o para colocï¿½-lo no modo executï¿½vel
+ * Meios de "tentar" (não há garantias) de retirar um Thread de execução:
+ * yield() -> solicita que o sheduler retire o thread em execução para colocá-lo no modo executável
  * sleep() -> solicita um tempo de sleep para o thread atual (deve ser inserido dentro de um bloco try/catch com exception do tipo InterruptedException)
  * 
  * Meio garantido
- * join() -> determina que um Thread sï¿½ pode ser executado apï¿½s a conclusï¿½o de outro 
- * Verificar o mï¿½todo abaixo testeJoin() para verificar o funcionamento
+ * join() -> determina que um Thread só pode ser executado após a conclusão de outro 
+ * Verificar o método abaixo testeJoin() para verificar o funcionamento
  */
 public class Testthread implements Runnable{
 
@@ -26,29 +26,29 @@ public class Testthread implements Runnable{
 			System.out.println("Executando a thread " + Thread.currentThread().getName() + " i = " + i);
 			
 			// metodo yield()
-			// Nao garantias, mas o metodo yield() tenta fazer com que o Thread corrente 
-			// dï¿½ lugar a outro thread
-			//Thread.yield();
+			// O método yield() tenta fazer com que o Thread corrente dê lugar a outro thread - não há garantias
+			Thread.yield();
 			
 			/*
-			 * dando um sleep de 1 segundo (parametro passado em milï¿½ssimos de segundos)
+			 * dando um sleep de 1 segundo (parametro passado em miléssimos de segundos)
 			 */
-			/*try {
+			try {
 				Thread.sleep(1000 * 1);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			}*/
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
 		testeJoin();
+		//iniciarSomenteUmaVez();
 	}
 	
 
 	/**
-	 * ao iniciar esta execuï¿½ï¿½o, mesmo estabelecendo prioridades,
-	 * nï¿½o hï¿½ garantias
+	 * ao iniciar esta execução, mesmo estabelecendo prioridades,
+	 * não há garantias
 	 */
 	public static void rodandoThreads(){
 		for (int i = 0; i < 10; i++){
@@ -61,8 +61,8 @@ public class Testthread implements Runnable{
 	}
 
 	/**
-	 * Uma vez iniciada uma thread, ela JAMAIS serï¿½ iniciada novamente
-	 * Lanï¿½arï¿½ java.lang.IllegalThreadStateException mas a Thread executarï¿½ uma vez normalmente
+	 * Uma vez iniciada uma thread, ela JAMAIS será iniciada novamente
+	 * Lançará java.lang.IllegalThreadStateException mas a Thread executará uma vez normalmente
 	 */
 	public static void iniciarSomenteUmaVez(){
 		Testthread test = new Testthread();
@@ -76,16 +76,16 @@ public class Testthread implements Runnable{
 		Testthread t2 = new Testthread();
 		Thread T1 = new Thread(t1);
 		Thread T2 = new Thread(t2);
-		try {
-			// traduï¿½ï¿½o do join() abaixo: T2, junte-se ao final de T1. Enquanto T1 nï¿½o
-			// acabar, nï¿½o "volte" a executar (ou nï¿½o inicie caso nï¿½o tenha startado ainda).
+/*		try {
+			// tradução do join() abaixo: T2, junte-se ao final de T1. Enquanto T1 não
+			// acabar, não "volte" a executar (ou não inicie caso não tenha startado ainda).
 			// Interessante observar que se T2.start() for feito primeiro uma parte de T2
-			// poderï¿½ executar antes de iniciar T1. Quando T1 for iniciado, aï¿½ sim T2 sï¿½ 
-			// voltarï¿½ a ser executado ao final de T1
+			// poderá executar antes de iniciar T1. Quando T1 for iniciado, aí sim T2 só 
+			// voltará a ser executado ao final de T1
 			T2.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}
+		}*/
 		T1.start();
 		T2.start();
 	}
