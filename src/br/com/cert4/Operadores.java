45259c1cb5d50c2edf3ada7a4ce91b2719e7edb8
Leandro Souza <leandro.asouza@spread.com.br>
2013-01-31 13:13:21 -0200

    Diversas alterações resumos e codigo cap4-Operadores.java

--- src/br/com/cert4/Operadores.java
+++ src/br/com/cert4/Operadores.java
@@ -12,6 +12,33 @@ public class Operadores {
 	}
 	
 	public static void main(String[] args) {
-		new Operadores().prePosIncremento();
+		//new Operadores().prePosIncremento();
+		new Operadores().testeSwitch();
+	}
+	
+	/*
+	 * O teste abaixo informa o valor 642, visto que 1 foi passado ao switch. 
+	 * Ao testar todas as opções o JVM verifica que a única opção válida é a 
+	 * default pois não há correspondência de valores. Após o default, até que 
+	 * seja encontrado o próximo break, todas as instruções serão executadas 
+	 * dentro de outros cases.
+	 */
+	public void testeSwitch(){
+		int a = 1;
+		
+		switch(a){
+			case 2: 
+				System.out.print("2"); 
+				break;
+			default:
+				System.out.print("6");
+				// nao existe break aqui
+			case 4:
+				System.out.print("4");
+				// nao existe break aqui
+			case 5:
+				System.out.print("5");
+				break;
+		}
 	}
 }
